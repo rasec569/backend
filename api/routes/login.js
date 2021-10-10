f24bf734c182fdfd8782252cd191393944abde20
rasec569 <cesaraugusto_569@hotmail.com>
2021-10-10 08:47:10 -0500

    correcion validacion usuario

--- api/routes/login.js
+++ api/routes/login.js
@@ -1,31 +1,29 @@
-const express= require('express');
-const router= express.Router();
+const express = require("express");
+const router = express.Router();
 // invoca la conexion
-const conexion = require('../config/conexion');
+const conexion = require("../config/conexion");
 
 // token para las peticiones a mysql
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 //validar usuario
-router.post('/', (req,res) => {
-    const { Usuario, password } = req.body;
-    conexion.query(`CALL ConsultarSesionUsuario('${Usuario}', '${password}')`, 
-    [Usuario, password ], 
-    (err, rows, fields)=>{
-        if(!err){
-            if(rows.length>0){                
-                let data=JSON.stringify(rows[0]);
-                const token= jwt.sign(data,'MCG');
-                res.json({token});
-            }else{
-                res.json('Usuario o contraseña incorrectos');                
-            }
-          }else{
-            console.log(err);
-          }
+router.post("/", (req, res) => {
+  const { Usuario, password } = req.body;
+  let sql = `CALL ConsultarSesionUsuario('${Usuario}', '${password}')`;
+  conexion.query(sql, [Usuario, password], (err, rows, fields) => {
+    /* console.log(sql); */
+    if (!err) {
+      if (rows[0].length > 0) {
+        let data = JSON.stringify(rows[0]);
+        const token = jwt.sign(data, "MCG");
+        res.json({ token });
+      } else {
+        res.json("Usuario o contraseña incorrectos");
+      }
+    } else {
+      console.log(err);
     }
-    
-    )
+  });
 });
 
 module.exports = router;
